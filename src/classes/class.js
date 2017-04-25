7b415e886b0d983ac533eaa51ce3840a6191fc87
James Bruce <jamesjbruce@gmail.com>
2017-04-25 20:14:00 +0000

    Add the first react-class for rendering

---
+++ src/classes/class.js
@@ -0,0 +1,20 @@
+var weapon = React.createClass({
+    propTypes: {
+        ItemName:       React.PropTypes.string.isRequired,
+        DisplayName:    React.PropTypes.string.isRequired,
+        Recipe:         React.PropTypes.any,
+        Source:         React.PropTypes.string,
+        Description:    React.PropTypes.string,
+    },
+    
+    render: function() {
+        return (
+            React.createElement('div', {key:        this.props.ItemName,
+                                        className:  this.props.ItemName,
+                                       },
+                                React.createElement('div', {}, "Name: " + this.props.DisplayName),
+                                React.createElement('div', {}, "Description: " + this.props.Description)
+            )
+        )
+    },
+});
